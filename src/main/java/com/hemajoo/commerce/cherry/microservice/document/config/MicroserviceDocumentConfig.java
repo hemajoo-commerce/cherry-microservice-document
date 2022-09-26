e530d1544e5ce6dc526048d30b59520c13a56dc4
Resse Christophe <christophe.resse@gmail.com>
2022-09-26 17:41:11 +0200

    chore(config): Adding microservice Spring configuration

---
+++ src/main/java/com/hemajoo/commerce/cherry/microservice/document/config/MicroserviceDocumentConfig.java
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright Hemajoo Systems Inc.  2022 - All Rights Reserved
+ * -----------------------------------------------------------------------------------------------
+ * All information contained herein is, and remains the property of
+ * Hemajoo Inc. and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Hemajoo Inc. and its
+ * suppliers and may be covered by U.S. and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained from
+ * Hemajoo Systems Inc.
+ * -----------------------------------------------------------------------------------------------
+ */
+package com.hemajoo.commerce.cherry.microservice.document.config;
+
+import com.hemajoo.commerce.cherry.base.data.model.document.Document;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackageClasses = { Document.class })
+@EnableJpaRepositories
+public class MicroserviceDocumentConfig
+{
+}
